8f434f07ffccf93cd96f9b4b361b1b8650bd159f
Harshita Raj <harshitarajj@gmail.com>
2021-01-04 23:27:42 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -9,20 +9,20 @@ function setup() {
   var bottomGrass1 = createSprite(683,height-50,width,grassHeight);
   bottomGrass1.shapeColor = "green";
 
-  var bottomGrass5 = createSprite(683,height-450,width,grassHeight);
-  bottomGrass5.shapeColor = "green";
+  var bottomGrass2 = createSprite(683,height-450,width,grassHeight);
+  bottomGrass2.shapeColor = "green";
   
-  var bottomGrass9 = createSprite(683,height-850,width,grassHeight);
-  bottomGrass9.shapeColor = "green";
+  var bottomGrass3 = createSprite(683,height-850,width,grassHeight);
+  bottomGrass3.shapeColor = "green";
 
-  var bottomGrass13 = createSprite(683,height-1250,width,grassHeight);
-  bottomGrass13.shapeColor = "green";
+  var bottomGrass4 = createSprite(683,height-1250,width,grassHeight);
+  bottomGrass4.shapeColor = "green";
   
-  var bottomGrass17 = createSprite(683,height-1650,width,grassHeight);
-  bottomGrass17.shapeColor = "green";
+  var bottomGrass5 = createSprite(683,height-1650,width,grassHeight);
+  bottomGrass5.shapeColor = "green";
  
-  bottomGrass21 = createSprite(683,height-2050,width,grassHeight);
-  bottomGrass21.shapeColor = "green";
+  bottomGrass6 = createSprite(683,height-2050,width,grassHeight);
+  bottomGrass6.shapeColor = "green";
 }
 
 function draw() {
